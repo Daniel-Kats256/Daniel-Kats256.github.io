a6c09a10f989c9eb403e8ba5075971cec7ead694
senyonjodaniel <senyonjodaniel6@gmail.com>
2022-11-18 11:24:11 +0300

    initial commit

---
+++ mart3/events.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pryce concepts-events and parties</title>
+    <style>
+    <?php
+    require 'css/events.css';
+    require 'css/home.css';
+    require 'css/media.css';
+    ?>
+    </style>
+</head>
+<body>
+    <section class="etop">
+        <div class="title">
+            <h1>Pryce<span>Concepts</span></h1>
+            <p>Decoration and Events Management</p>
+        </div>
+        <div class="navi">
+            <nav>
+                <a href="index.php">HOME</a>
+                <a href="events.php">EVENTS</a>
+                <a href="news.php">NEWS</a>
+                <div class="about1">
+                        About Us
+                        <div class="icon1">
+                             <span><?php include"icons/angle-right.svg"?></span>
+                        </div>
+                        <div class="drop3">
+                           <nav>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/neuter.svg"?></span>
+                                </div>
+                                company policy
+                            </a>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/circle-user.svg"?></span>
+                                 </div>
+                                company profile
+                            </a>
+                            <a href="#">
+                               <div class="icon1">
+                                 <span><?php include"icons/network-wired.svg"?></span>
+                                 </div>
+                                Origin and founder
+
+                            </a>
+                           </nav>
+                        </div>
+                </div>
+                <a href="#">CONTACTS</a>
+            </nav>
+            <div class="icons">
+                <div class="icon">
+                    <span><?php include"icons/magnifying-glass.svg"?></span>
+                </div>
+            </div>
+        </div>
+        <div class="null">
+             <div class="box">
+                    <img src="images/logo.png" alt="">
+             </div>
+             <p>Your Favourite Spot</p>
+             <div class="icon">
+                 <span><?php include"icons/shuffle.svg"?></span>
+             </div>
+        </div>
+    </section>
+    <section class="middle1">
+        <div class="scrollimg">
+            <div class="bike">
+                <p>Ballon Decoration full of beauty that will make your celebrations memorable</p>
+                <img src="images/IMG-20220807-WA0007.jpg" alt="bike">
+            </div>
+            <div class="boxes">
+                <div>
+                    <img src="images/IMG-20220411-WA0018.jpg" alt="">
+                </div>
+                <div>
+                    <img src="images/IMG-20220411-WA0017.jpg" alt="">
+                </div>
+                <div>
+                    <img src="images/IMG-20220411-WA0016.jpg" alt="">
+                </div>
+                <div>
+                    <img src="images/IMG-20220930-WA0024.jpg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="longimg">
+            <p> Have beautifully Detailed Photo booths </p>
+            <img src="images/IMG-20220930-WA0062.jpg" alt="">
+        </div>
+        <div class="doubleimg">
+            <div>
+                <p>Well stocked Table Setting</p>
+                <img src="images/IMG-20220930-WA0060.jpg" alt="">
+            </div>
+            <div>
+                <p>And Pargola Tents for outside setting</p>
+                <img src="images/IMG-20220930-WA0064.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <div class="business">
+        <div class="bar">
+            <div></div>
+            <div>PACKAGES</div>
+        </div>
+        <div class="text">
+            <div class="icon">
+                 <span><?php include"icons/circle-half-stroke.svg"?></span>
+             </div>
+
+            <p>Open 24/7</p>
+        </div>
+    </div>
+    <section class="bottom">
+        <div class="people">
+            <div class="person">
+                <img src="images/IMG-20220930-WA0032.jpg" alt="">
+                <p>Ballon Decorations at the most affordable prices</p>
+                <div class="icons">
+                   
+                    <div class="stars" style="width: 80px;">
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star-half-stroke.svg"?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="boxes">
+                <div>
+                    <img src="images/six.jpg" alt="">
+                    <div>
+                        <p>Introduction Decoration</p>
+                        
+                        <div class="stars" style="width: 80px;">
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star-half-stroke.svg"?></span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="images/IMG-20220930-WA0058.jpg" alt="">
+                    <div>
+                        <p>Church Decoration</p>
+
+                        <div class="stars" style="width: 80px;">
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star-half-stroke.svg"?></span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src="images/IMG-20220930-WA0043.jpg" alt="">
+                    <div>
+                        <p>Engagement Decoration</p>
+
+                        <div class="stars" style="width: 80px;">
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star.svg"?></span>
+                        </div>
+                        <div class="icon">
+                                <span><?php include"icons/star-half-stroke.svg"?></span>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+        <div class="building">
+            <div>
+                <img src="images/IMG-20220930-WA0045.jpg" alt="">
+            </div>
+            <p>Decoration in Tranforming Lives <br>At the Most Affordable Prices</p>
+            <div>
+                <img src="images/IMG-20220930-WA0042.jpg" alt="">
+            </div>
+        </div>
+        <div class="advert">
+            <div>
+                <p>OTHER DECORATION COMPANIES</p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div class="icon">
+                            <img src="images/Deco 20221103_085348.jpg">
+                        </div>
+                        
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Deco 20221103_085348.jpg" alt="">
+                        </div>
+                         
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Mahad 20221103_085457.jpg">
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Mozey D 20221103_085854.jpg" alt="">
+                        </div>
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Rasta Dd 20221103_085720.jpg" alt="">
+                        </div>
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Emma Dd 20221103_085541.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Mahad 20221103_085457.jpg">
+                        </div>
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Mahad 20221103_085457.jpg" alt="">
+                        </div>
+                    </div>
+                    <div>
+                    <div class="icon">
+                            <img src="images/Mahad 20221103_085457.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="contact" style="height: 60px;">
+            <a href="contact.html">
+                Contact Us
+                <div class="icon">
+                    <span><?php include"icons/phone.svg"?></span>
+                </div>
+            </a>
+            <p> 0700966054 <br> 0754975422</p>
+            
+        </div>
+        <div class="follow">
+            <p>You can also follow us on our social media pages</p>
+            <div class="social">
+                <div>
+                    <div class="icon">
+                         <span><?php include"icons/facebook.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                        <span><?php include"icons/instagram.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                      <span><?php include"icons/whatsapp.svg"?></span>
+                    </div>
+                </div>
+                <div>
+                    <div class="icon">
+                        <span><?php include"icons/twitter.svg"?></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="quicklinks">
+            <p>Quick Links</p>
+            <div>
+                <a href="#">Location and contacts</a>
+                <a href="events.php">Events</a>
+                <a href="#">company profile</a>
+                <a href="#">Take a Tour</a>
+            </div>
+        </div>
+        <div class="copy">
+        
+           <p> &copy;2022 Pryce concepts</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ mart3/news.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pryce concepts-events and parties</title>
+    <style>
+    <?php
+     require 'css/news.css';
+     require 'css/home.css';
+     require 'css/media.css';
+    ?>
+    </style>
+</head>
+<body> 
+   <div class="body"> 
+    <div class="left">
+        <div>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="events.php">Events</a>
+                <a href="news.php">News</a>
+                <div class="dropdown2">
+                    About Us
+                    <div class="about2">
+                       <nav>
+                        <a href="#">names</a>
+                        <a href="#">hobbies</a>
+                        <a href="#">wea u stay</a>
+                       </nav>
+                    </div>
+                </div>
+                <a href="#">Contact Us</a>
+            </nav>
+        </div>
+    </div>
+    <div class="right2">
+        <div class="top1">
+            <div class="logo1">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <div class="history">
+                <p>Founders Of Pryce concepts</p>
+                <p>Explore Our Effective Team</p>
+            </div>
+        </div>
+        <div class="lorem">
+            <div class="bgd">
+                <img src="images/logo.png" alt="">
+                <div class="opac"></div>
+                <div class="content">
+                    <div class="person">
+                        <div class="pic">
+                            <img src="images/c27105a5ff504ea693352985e12d7504.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="name">CEO Pryce Concepts</div>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia voluptatum excepturi laborum asperiores odio dicta?</p>
+                            <div class="social">
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/facebook.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/instagram.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                    <span><?php include"icons/whatsapp.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/twitter.svg"?></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p> 0700966054  0754975422</p>
+                        </div>
+                    </div>
+                    <div class="person">
+                        <div class="pic">
+                            <img src="images/IMG-20220227-WA0158.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="name">Manager Pryce Concepts</div>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia voluptatum excepturi laborum asperiores odio dicta?</p>
+                            <div class="social">
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/facebook.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/instagram.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                    <span><?php include"icons/whatsapp.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/twitter.svg"?></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p> 0700966054  0754975422</p>
+                        </div>
+                    </div>
+                    <div class="person">
+                        <div class="pic">
+                            <img src="images/2021_02_17_22_44_IMG_9329.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="name">CEO Delight Decor</div>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia voluptatum excepturi laborum asperiores odio dicta?</p>
+                            <div class="social">
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/facebook.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/instagram.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                    <span><?php include"icons/whatsapp.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/twitter.svg"?></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p> 0700966054  0754975422</p>
+                        </div>
+                    </div>
+                    <div class="person">
+                        <div class="pic">
+                            <img src="images/IMG-20220930-WA0030.jpg" alt="">
+                        </div>
+                        <div class="info">
+                            <div class="name">Business Partner</div>
+                            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia voluptatum excepturi laborum asperiores odio dicta?</p>
+                            <div class="social">
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/facebook.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/instagram.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                    <span><?php include"icons/whatsapp.svg"?></span>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div class="icon">
+                                        <span><?php include"icons/twitter.svg"?></span>
+                                    </div>
+                                </div>
+                            </div>
+                            <p> 0700966054  0754975422</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+   </div>
+   <footer>
+    <div class="contact">
+        <a href="contact.html">
+            Contact Us
+            <div class="icon">
+                <span><?php include"icons/phone.svg"?></span>
+            </div>
+        </a>
+        <p> 0700966054 <br> 0754975422</p>
+        
+    </div>
+    <div class="follow">
+        <p>You can also follow us on our social media pages</p>
+        <div class="social">
+            <div>
+                <div class="icon">
+                     <span><?php include"icons/facebook.svg"?></span>
+                </div>
+            </div>
+            <div>
+                <div class="icon">
+                    <span><?php include"icons/instagram.svg"?></span>
+                </div>
+            </div>
+            <div>
+                <div class="icon">
+                  <span><?php include"icons/whatsapp.svg"?></span>
+                </div>
+            </div>
+            <div>
+                <div class="icon">
+                    <span><?php include"icons/twitter.svg"?></span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="quicklinks">
+        <p>Quick Links</p>
+        <div>
+            <a href="#">Location and contacts</a>
+            <a href="#">Events</a>
+            <a href="#">company profile</a>
+            <a href="#">Take a Tour</a>
+        </div>
+    </div>
+    <div class="copy">
+       <p> &copy;2022 Pryce concepts</p>
+    </div>
+</footer>
+</body>
+</html>
